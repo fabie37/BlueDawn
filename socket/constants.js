a6a323aba3a985fd4610a0edd51d44b1cef91e18
Fabrizio <fabiecatinella@gmail.com>
2021-01-28 11:30:56 +0000

    [feat] be able to quickly launch between dev and prod

--- socket/constants.js
+++ socket/constants.js
@@ -1,4 +1,13 @@
-const ORIGIN = 'http://192.168.1.217:3000';
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+let ORIGIN = '';
+if (process.env.NODE_ENV === 'developement') {
+    ORIGIN = 'http://localhost:3000';
+    console.log('Dev');
+} else {
+    ORIGIN = 'http://192.168.1.217:3000';
+    console.log('Prod');
+}
 
 const SEND_PURCHASES = 'send_purchases';
 
