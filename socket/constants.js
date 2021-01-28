33204791357b936d8dfb15614f1aba738274be05
Fabrizio <fabiecatinella@gmail.com>
2021-01-28 12:52:26 +0000

    [fix] global vars fixed

--- socket/constants.js
+++ socket/constants.js
@@ -1,15 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
-let ORIGIN = '';
-if (process.env.NODE_ENV === 'developement') {
-    ORIGIN = 'http://localhost:3000';
-    console.log('Dev');
-} else {
-    ORIGIN = 'http://192.168.1.217:3000';
-    console.log('Prod');
-}
 
 const SEND_PURCHASES = 'send_purchases';
 
-exports.ORIGIN = ORIGIN;
+exports.ORIGIN = process.env.SOCKET_URI;
 exports.SEND_PURCHASES = SEND_PURCHASES;
